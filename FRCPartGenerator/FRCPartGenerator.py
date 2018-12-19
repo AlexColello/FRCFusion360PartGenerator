32cc128cdf7e6ec375adf38723aa3d14ee1ef64a
AlexColello <acolello@usc.edu>
2018-12-19 15:51:58 -0800

    Initial commit

---
+++ FRCPartGenerator/FRCPartGenerator.py
@@ -0,0 +1,15 @@
+#Author-Alex Colello
+#Description-Generates a variety of commonly used parts for the FIRST Robotics Competition.
+
+import adsk.core, adsk.fusion, adsk.cam, traceback
+
+def run(context):
+    ui = None
+    try:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        ui.messageBox('Hello script')
+
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
